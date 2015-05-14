7ebe79fab92f99ed7b7e64bc4702e9bce64d179a
miltonneighbour <yiminggan@yahoo.com>
2015-05-14 22:24:08 +0200

    merged

--- CRISPR-Genomics/count-spacer-occurrence-in-genome.py
+++ CRISPR-Genomics/count-spacer-occurrence-in-genome.py
@@ -8,62 +8,54 @@ from difflib import get_close_matches, SequenceMatcher
 # pilercrDir = file path of pilercr folder
 # genomePath = file path of genome, function will look for spacers in this genome, FASTA format
  
-pilerIn = "../ICP1.fasta"
-pilerOut = "../output.txt"
-pilercrDir = "/Users/yiminggan/Desktop/pilercr1.06"
-genomePath = "/Users/yiminggan/Desktop/vpICP1_2011_A.fasta" 
+pilerDir = "crispr/"
+pilerOut = "crispr/%s"
+genomePath = "genomes/" 
+crisprHits = "crispr-hits/"
+spacers_regex = re.compile('(?<=\.\.\.    )\w+')
 
 ##### Find CRISPR using pilercr #####
-os.chdir(pilercrDir)
-cmd = "./pilercr -in %s -out %s"%(pilerIn,pilerOut)
-os.system(cmd) 
-
+for fn in os.listdir(genomePath):
+        cmd = "pilercr -in %s -out %s"%(genomePath + fn,pilerOut % fn)
+        os.system(cmd) 
 
 ##### Get spacers from output.txt #####
-report = open(pilerOut, 'r')
-report = report.read()
-spacers = re.findall('[ATCG]{30}', report)
-
-
-##### Read genome #####
-genome = open(genomePath,'r')
-
-# Join genome in one string, removing first line
-genome = genome.readlines()[1:]
-genome = "".join(genome)
-genome = genome.replace('\n', "")
-
-
-##### Count spacer occurrence in genome #####
-print "\n\nSpacers from: " + pilerIn
-print "Genome from: " + genomePath
-print "==================== RESULTS ====================\n"
-
-for spacer in spacers:
-	n = genome.count(spacer)
-	print "Spacer: {}	Count: {}".format(spacer, n) 
-
+for fn in os.listdir(pilerDir):
+        report = open(pilerDir + fn, 'r')
+        report = report.read()
+        spacers = re.findall(spacers_regex, report)
+        ##### Read genome #####
+        g = open(genomePath + fn, 'r')
+        print fn
+        genome = g.readlines()[1:]
+        genome = "".join(genome)
+        genome = genome.replace('\n', "")
+        ##### Count spacer occurrence in genome and save output #####
+        for spacer in spacers:
+                n = genome.count(spacer)
+                print "Spacer: %s\tOccurrence: %i" % (spacer, n) 
 
 ##### Count loose match of spacer in genome #####
-print "\n\nSequences similar to spacers\n"
-print "************ Spacer ************ Match ************ Similarity ************"
+# print "\n\nSequences similar to spacers\n"
+# print "************ Spacer ************ Match ************ Similarity ************"
 
-for spacer in spacers:
-	i = 0
-	index = genome.find(spacer)
-	interval = index + len(spacer)
-	window = index - len(spacer)	
+# for spacer in spacers:
+# 	i = 0
+# 	index = genome.find(spacer)
+# 	interval = index + len(spacer)
+# 	window = index - len(spacer)	
 	
-	for nucleotide in genome:
-		i = i + 1
-		if i < window or i > interval:
-			seq = genome[i:i+window]
-			match = SequenceMatcher(None, spacer, seq).ratio()
-			if match > 0.80:
-				print spacer + '\t' + seq + '\t' + str(match)
-			else:
-				continue
-		else:
-			continue
+# 	for nucleotide in genome:
+# 		i = i + 1
+# 		if i < window or i > interval:
+# 			seq = genome[i:i+window]
+# 			match = SequenceMatcher(None, spacer, seq).ratio()
+# 			if match > 0.80:
+# 				print spacer + '\t' + seq + '\t' + str(match)
+# 			else:
+# 				continue
+# 		else:
+# 			continue
 
-print "\t\t\t\t ***** END *****"
+# print "\t\t\t\t ***** END *****"
+
