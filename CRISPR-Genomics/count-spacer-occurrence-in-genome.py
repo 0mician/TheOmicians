7a5c004d0366fd395bd682b2856cc986bc8c4343
Milt0n <cedric.lood@gmail.com>
2015-05-14 21:59:10 +0200

    modifications to include looping over multiple genomes

--- CRISPR-Genomics/count-spacer-occurrence-in-genome.py
+++ CRISPR-Genomics/count-spacer-occurrence-in-genome.py
@@ -7,37 +7,30 @@ import re
 # pilercrDir = file path of pilercr folder
 # genomePath = file path of genome, function will look for spacers in this genome, FASTA format
  
-pilerIn = "../somesequence.fasta"
-pilerOut = "../output.txt"
-pilercrDir = "/somepath/pilercr1.06"
-genomePath = "/somepath/somegenome.fasta" 
-
+pilerDir = "crispr/"
+pilerOut = "crispr/%s"
+genomePath = "genomes/" 
+crisprHits = "crispr-hits/"
+spacers_regex = re.compile('(?<=\.\.\.    )\w+')
 
 ##### Find CRISPR using pilercr #####
-os.chdir(pilercrDir)
-cmd = "./pilercr -in %s -out %s"%(pilerIn,pilerOut)
-os.system(cmd) 
-
+for fn in os.listdir(genomePath):
+        cmd = "pilercr -in %s -out %s"%(genomePath + fn,pilerOut % fn)
+        os.system(cmd) 
 
 ##### Get spacers from output.txt #####
-report = open(pilerOut, 'r')
-report = report.read()
-spacers = re.findall('[ATCG]{30}', report)
+for fn in os.listdir(pilerDir):
+        report = open(pilerDir + fn, 'r')
+        report = report.read()
+        spacers = re.findall(spacers_regex, report)
+        ##### Read genome #####
+        g = open(genomePath + fn, 'r')
+        print fn
+        genome = g.readlines()[1:]
+        genome = "".join(genome)
+        genome = genome.replace('\n', "")
+        ##### Count spacer occurrence in genome and save output #####
+        for spacer in spacers:
+                n = genome.count(spacer)
+                print "Spacer: %s\tOccurrence: %i" % (spacer, n) 
 
-
-##### Read genome #####
-genome = open(genomePath,'r')
-
-# Join genome in one string, removing first line
-genome = genome.readlines()[1:]
-genome = "".join(genome)
-genome = genome.replace('\n', "")
-
-
-##### Count spacer occurrence in genome #####
-for spacer in spacers:
-	n = genome.count(spacer)
-	print "Spacer: {}	Occurrence: {}".format(spacer, n) 
-
-
-##### Count loose match of spacer in genome #####
