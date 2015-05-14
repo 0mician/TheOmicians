6fcc66e4cf1f141413f1e9830ab4e8885dfc7e24
Milt0n <cedric.lood@gmail.com>
2015-05-14 22:08:03 +0200

    merged files, commented loose search for later use

--- CRISPR-Genomics/count-spacer-occurrence-in-genome.py
+++ CRISPR-Genomics/count-spacer-occurrence-in-genome.py
@@ -1,5 +1,6 @@
 import os
 import re
+from difflib import get_close_matches, SequenceMatcher
 
 ##### Specify file path #####
 # pilerIn = file path of input for pilerCR
@@ -33,4 +34,28 @@ for fn in os.listdir(pilerDir):
         for spacer in spacers:
                 n = genome.count(spacer)
                 print "Spacer: %s\tOccurrence: %i" % (spacer, n) 
+
+##### Count loose match of spacer in genome #####
+# print "\n\nSequences similar to spacers\n"
+# print "************ Spacer ************ Match ************ Similarity ************"
+
+# for spacer in spacers:
+# 	i = 0
+# 	index = genome.find(spacer)
+# 	interval = index + len(spacer)
+# 	window = index - len(spacer)	
+	
+# 	for nucleotide in genome:
+# 		i = i + 1
+# 		if i < window or i > interval:
+# 			seq = genome[i:i+window]
+# 			match = SequenceMatcher(None, spacer, seq).ratio()
+# 			if match > 0.80:
+# 				print spacer + '\t' + seq + '\t' + str(match)
+# 			else:
+# 				continue
+# 		else:
+# 			continue
+
+# print "\t\t\t\t ***** END *****"
 
