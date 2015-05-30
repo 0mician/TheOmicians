a18e4b9555e4f3a11e0aaf819e8670f0e54c7c11
Milt0n <cedric.lood@gmail.com>
2015-05-30 09:43:27 +0200

    integration of another filtering step

--- CRISPR-Genomics/count-spacers-occurrences-in-genomes.py
+++ CRISPR-Genomics/count-spacers-occurrences-in-genomes.py
@@ -9,7 +9,7 @@ from difflib import get_close_matches, SequenceMatcher
 # crisprHits = if multiple crispr spacers are found in a genome (outside of CRISPR region)
 
 pilerDir = "crispr/"
-genomePath = "genomes/" 
+genomePath = "genomes_full/" 
 spacers_regex = re.compile('(?<=\.\.\.    )\w+')
 report_out = open("spacer_count.txt", "w")
 
@@ -38,45 +38,21 @@ for fn in os.listdir(pilerDir):
     genome = genome.replace('\n', "")
 
     ### Count spacer occurrence in genome
-    flag_file = False
     spacer_dic = {}
     for spacer in spacers:
         n = genome.count(spacer)
         if(n > 1 and len(spacer) > 15):
-            if(flag_file == False):
-                report_out.write(fn + "\n")
-                flag_file = True
             spacer_dic[spacer] = n
 
     ### Filter out spacers that are repeated in the CRISPR regions
+    flag_file = False
     for key in spacer_dic:   
         if (count[key] != spacer_dic[key]):
+            if(flag_file == False):
+                report_out.write("#####" + "\n")
+                report_out.write(fn + "\n")
+                flag_file = True
             report_out.write("Spacer: %s\tOccurrence: %i \n" % (key, spacer_dic[key]))
             
 report_out.close()
  
-
-##### Count loose match of spacer in genome #####
-# print "\n\nSequences similar to spacers\n"
-# print "************ Spacer ************ Match ************ Similarity ************"
-
-# for spacer in spacers:
-# 	i = 0
-# 	index = genome.find(spacer)
-# 	interval = index + len(spacer)
-# 	window = index - len(spacer)	
-	
-# 	for nucleotide in genome:
-# 		i = i + 1
-# 		if i < window or i > interval:
-# 			seq = genome[i:i+window]
-# 			match = SequenceMatcher(None, spacer, seq).ratio()
-# 			if match > 0.80:
-# 				print spacer + '\t' + seq + '\t' + str(match)
-# 			else:
-# 				continue
-# 		else:
-# 			continue
-
-# print "\t\t\t\t ***** END *****"
-
